32790da1b2819e2a87dcf3a20f23efa67fec2268
Justyna <justyna.e.pinkowska@gmail.com>
2025-02-09 19:30:39 +0000

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,3 +1,9 @@
+#Bank currency
+#Prompt the user and read in two money amounts (in cent)
+#Add the two amounts
+#Print out the answer in a human readable format with a euro sign and decimal point between the euro and cent of the amount
+#Justyna Pinkowska
+
 amount1 = 65  #in cent
 amount2 = 180 #in cent
 x = (amount1 + amount2)/ 100
