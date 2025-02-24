26c89b9f4513c5020d9c853fb0a5d752173e2160
Justyna Pinkowska <justyna.e.pinkowska@gmail.com>
2025-02-24 20:30:19 +0000

    collatz.py

---
+++ collatz.py
@@ -0,0 +1,22 @@
+# Program allows  user to input any positive integer and outputs the successive values of the following calculation.
+# At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one.
+# Ends if the current value is one. 
+# Author: Justyna Pinkowska
+
+positive_integer = int(input("Input any positive integer:"))
+print(positive_integer, end=" ")
+
+while positive_integer != 1:
+
+    if positive_integer % 2 == 0:
+        positive_integer = int(positive_integer / 2)
+        
+    else:
+        positive_integer = int(positive_integer * 3 + 1)
+    print(positive_integer, end=" ")
+
+
+     
+
+print() # prints space after each number instead of new line
+
