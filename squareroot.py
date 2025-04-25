644190d77a168a626bdc0355e5da991b3f5e2b89
Justyna <justyna.e.pinkowska@gmail.com>
2025-04-25 20:28:40 +0100

    Update squareroot.py

--- squareroot.py
+++ squareroot.py
@@ -1,29 +1,24 @@
 # Program that takes a positive floating-point number as input and outputs an approximation of its square root.
 # Author: Justyna Pinkowska
-
-
+# The Babylonian method
 
 def round_to_one_decimal(number): # Rounds a float to one decimal place
     return round(number, 1)
 
 n = float(input("Please enter a positive number: "))
 def sqrt(n):
+   if n < 0:
+        raise ValueError("Cannot calculate the square root of a negative number")
+    if n == 0:
+        return 0 
     
-<<<<<<< HEAD
-    approximate = n/2
-    new_approximate = (approximate + n/approximate)/2
-    while new_approximate != approximate:
-        approximate = new_approximate
-        new_approximate = (approximate + n/approximate)/2
-    return approximate
-=======
-    approx = n/2
-    next_approx = (approx + n/approx)/2
-    while next_approx != approx:
+    approx = n/2 # initial guess for the square root of n
+    next_approx = (approx + n/approx)/2  # calculates approximation by averaging the current guess (approx) with n divided by the current guess
+    while next_approx != approx:  #  will continue to execute as long as the new_approximate calculated in the previous step is not equal to the approximate
         approx = next_approx
-        next_approx = (approx + n/approx)/2
+        next_approx = (approx + n/approx)/2 # recalculates a new napprox using the updated approx, repeats until the condition in the while loop becomes false
     return approx
->>>>>>> b4bc0eaec7bb35d43213ff158a45d1589257419d
+
 
 x = round_to_one_decimal(sqrt(n))
 print(f"The square root of {n} is approx. {x}.")
